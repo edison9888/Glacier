1250777e8193fc1bc75b585c884c2ed11204979f
saldin <zhaocn@live.cn>
2012-09-26 22:37:16 +0800

    修改

--- GlacierApplication/GlacierApplication/MainPage/Controller/MainPageController.m
+++ GlacierApplication/GlacierApplication/MainPage/Controller/MainPageController.m
@@ -110,10 +110,13 @@
 {
     [super viewDidLoad];
     mCurrentJobType = 1;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
     self.plipdtm_list = [plipdtm findByCriteria:@"group by pdpdtcode"];
     self.pkclass_list = [pkclass findByCriteria:@"order by pk"];
     [self adjustCurrentPkClassList];
-
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
