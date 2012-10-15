520514cc0e2a16ba4d858eb12d9e33660d868876
saldin <zhaocn@live.cn>
2012-10-15 23:02:15 +0800

    修改日历方式

--- GlacierApplication/GlacierApplication/MainPage/Controller/PopDateController.m
+++ GlacierApplication/GlacierApplication/MainPage/Controller/PopDateController.m
@@ -9,32 +9,52 @@
 #import "PopDateController.h"
 
 @interface PopDateController ()
-
+@property (nonatomic,retain) NSDateFormatter * twDateFormatter;
 @end
 
 @implementation PopDateController
+{
+
+    IBOutlet UINavigationItem *mNavItem;
+}
 @synthesize picker;
 @synthesize popDateDelegate;
 @synthesize maxDate;
 @synthesize minDate;
 @synthesize selectedDate;
+@synthesize twDateFormatter;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) 
     {
+        NSDateFormatter * wTwDateFormatter = [[NSDateFormatter alloc] init];
+        twDateFormatter = wTwDateFormatter;
+        NSCalendar *wRepublicOfChinaCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSRepublicOfChinaCalendar];
+        self.twDateFormatter.calendar = wRepublicOfChinaCalendar;
         
+        NSLocale *wLocale  = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_TW"];
+        self.twDateFormatter.locale = wLocale;
+        
+        self.twDateFormatter.dateFormat = @"G yyy年MM月dd日";
     }
     return self;
+}
+
+- (IBAction)onDateChanged:(UIDatePicker *)sender
+{
+    [mNavItem setTitle:[self.twDateFormatter stringFromDate:sender.date]];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     if (self.selectedDate)
     {
         picker.date = self.selectedDate;
+        [mNavItem setTitle:[self.twDateFormatter stringFromDate:picker.date]];
     }
     picker.minimumDate = self.minDate;
     picker.maximumDate = self.maxDate;
@@ -56,11 +76,10 @@
     }
 }
 
-
-
 - (void)viewDidUnload
 {
     [self setPicker:nil];
+    mNavItem = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
